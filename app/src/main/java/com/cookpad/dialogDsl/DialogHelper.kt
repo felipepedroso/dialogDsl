0098fdb2390e51013214590fb5ebb565cfa6c7af
Felipe Pedroso <felipe-pedroso@cookpad.com>
2019-03-15 13:43:00 +0000

    First commit.

---
+++ app/src/main/java/com/cookpad/dialogDsl/DialogHelper.kt
@@ -0,0 +1,176 @@
+package com.cookpad.dialogDsl
+
+import android.content.Context
+import android.view.View
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+
+class DialogButtonInitializer {
+    @StringRes
+    var textResourceId: Int? = null
+
+    var text: String? = null
+
+    var onClickListener: () -> Unit = { }
+}
+
+class CookpadDialogInitializer {
+    @StringRes
+    var messageResourceId: Int? = null
+
+    var messageText: String? = null
+
+    @StringRes
+    var titleResourceId: Int? = null
+
+    var titleText: String? = null
+
+    var isCancelable: Boolean = true
+
+    var customView: View? = null
+
+    internal var positiveButtonInitializer: DialogButtonInitializer? = null
+
+    fun positiveButton(initializeAction: DialogButtonInitializer.() -> Unit) {
+        positiveButtonInitializer = DialogButtonInitializer().also(initializeAction)
+    }
+
+    internal var negativeButtonInitializer: DialogButtonInitializer? = null
+
+    fun negativeButton(initializeAction: DialogButtonInitializer.() -> Unit) {
+        negativeButtonInitializer = DialogButtonInitializer().also(initializeAction)
+    }
+
+    var onDismissListener: () -> Unit = { }
+
+    var onCancelListener: () -> Unit = { }
+}
+
+fun Context.buildDialog(
+    initializeAction: CookpadDialogInitializer.() -> Unit
+): AlertDialog {
+    val initializer = CookpadDialogInitializer()
+    initializeAction(initializer)
+
+    return AlertDialog.Builder(this)
+        .initialize(initializer)
+        .create()
+}
+
+fun Fragment.buildDialog(initializeAction: CookpadDialogInitializer.() -> Unit) =
+    this.context?.buildDialog(initializeAction)
+
+fun Context.showDialog(initializeAction: CookpadDialogInitializer.() -> Unit) =
+    buildDialog(initializeAction).show()
+
+fun Fragment.showDialog(initializeAction: CookpadDialogInitializer.() -> Unit) =
+    context?.showDialog(initializeAction)
+
+private fun AlertDialog.Builder.initialize(
+    initializer: CookpadDialogInitializer
+): AlertDialog.Builder {
+    with(initializer) {
+        assertProperInitialization(this)
+
+        messageText?.let {
+            setMessage(it)
+        }
+
+        messageResourceId?.let {
+            setMessage(it) // Using resources should always prevail.
+        }
+
+        titleText?.let {
+            setTitle(it)
+        }
+
+        titleResourceId?.let {
+            setTitle(it) // Using resources should always prevail.
+        }
+
+        setCancelable(isCancelable)
+
+        customView?.let {
+            setView(it)
+        }
+
+        positiveButtonInitializer?.let {
+            initializePositiveButton(it)
+        }
+
+        negativeButtonInitializer?.let {
+            initializeNegativeButton(it)
+        }
+
+        setOnDismissListener { onDismissListener() }
+
+        setOnCancelListener { onCancelListener() }
+    }
+
+    return this
+}
+
+fun AlertDialog.Builder.initializeNegativeButton(buttonInitializer: DialogButtonInitializer) {
+    val builder = this
+
+    with(buttonInitializer) {
+        text?.let {
+            builder.setNegativeButton(it) { dialog, _ ->
+                dialog.dismiss()
+                onClickListener()
+            }
+        }
+
+        textResourceId?.let {
+            builder.setNegativeButton(it) { dialog, _ ->
+                dialog.dismiss()
+                onClickListener()
+            }
+        }
+    }
+}
+
+private fun AlertDialog.Builder.initializePositiveButton(
+    buttonInitializer: DialogButtonInitializer
+) {
+    val builder = this
+
+    with(buttonInitializer) {
+        text?.let {
+            builder.setPositiveButton(it) { _, _ -> onClickListener() }
+        }
+
+        textResourceId?.let {
+            builder.setPositiveButton(it) { _, _ -> onClickListener() }
+        }
+    }
+}
+
+private fun assertProperInitialization(initializer: CookpadDialogInitializer) {
+    with(initializer) {
+        if (messageResourceId == null && messageText == null && customView == null) {
+            throw MissingMessageDuringDialogInitialization()
+        }
+
+        if (positiveButtonInitializer?.textResourceId == null && positiveButtonInitializer?.text == null && customView == null) {
+            throw MissingPositiveButtonDuringDialogInitialization()
+        }
+    }
+}
+
+private const val MISSING_MESSAGE_ERROR: String =
+    "Please ensure that your dialog initialization" +
+            " is setting the dialog content using \'messageText\', \'messageResourceId\' or \'customView\'"
+
+private const val MISSING_POSITIVE_BUTTON_INITIALIZATION_ERROR: String =
+    "Please ensure that your dialog initialization" +
+            " is setting the positive button text using \'messageText\' or \'messageResourceId\'" +
+            "the \'positiveButton\' tag."
+
+class MissingMessageDuringDialogInitialization : Throwable(MISSING_MESSAGE_ERROR)
+
+class MissingPositiveButtonDuringDialogInitialization :
+    Throwable(MISSING_POSITIVE_BUTTON_INITIALIZATION_ERROR)
+
+
---
+++ app/src/main/java/com/cookpad/dialogDsl/MainActivity.kt
@@ -0,0 +1,183 @@
+package com.cookpad.dialogDsl
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val MESSAGE_CLICKED_ON_POSITIVE_BUTTON = "The user clicked on the positive button!"
+        private const val MESSAGE_CLICKED_ON_NEGATIVE_BUTTON = "The user clicked on the negative button!"
+        private const val MESSAGE_DIALOG_DISMISSED = "The dialog was dismissed!"
+        private const val MESSAGE_DIALOG_CANCELED = "The dialog was canceled!"
+        private const val MESSAGE_CUSTOM_VIEW_INTERACTION = "The user interacted with custom view!"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupViews()
+    }
+
+    private fun setupViews() {
+        setContentView(R.layout.activity_main)
+
+        buttonAlertDialog.setOnClickListener {
+            displayAlertDialog()
+        }
+
+        buttonAlertDialogWithPositiveButtonCallback.setOnClickListener {
+            displayAlertDialogWithPositiveButtonCallback()
+        }
+
+        buttonAlertDialogWithPositiveAndNegativeButtonsCallbacks.setOnClickListener {
+            displayAlertDialogWithAlertDialogWithPositiveAndNegativeButtonsCallbacks()
+        }
+
+        buttonAlertDialogWithDismissCallback.setOnClickListener {
+            displayAlertDialogWithDismissCallback()
+        }
+
+        buttonAlertDialogWithCancelCallback.setOnClickListener {
+            displayAlertDialogWithCancelCallback()
+        }
+
+        buttonAlertDialogWithCustomView.setOnClickListener {
+            displayAlertDialogWithCustomView()
+        }
+    }
+
+    private fun displayAlertDialog() {
+        resetDialogResult()
+
+        showDialog {
+            messageResourceId = R.string.alert_dialog_message
+
+            positiveButton {
+                textResourceId = android.R.string.ok
+            }
+        }
+    }
+
+    private fun displayAlertDialogWithPositiveButtonCallback() {
+        resetDialogResult()
+
+        showDialog {
+            messageResourceId = R.string.alert_dialog_message
+
+            positiveButton {
+                textResourceId = android.R.string.ok
+                onClickListener = {
+                    updateResultDialog(MESSAGE_CLICKED_ON_POSITIVE_BUTTON)
+                }
+            }
+        }
+    }
+
+    private fun displayAlertDialogWithAlertDialogWithPositiveAndNegativeButtonsCallbacks() {
+        resetDialogResult()
+
+        showDialog {
+            messageResourceId = R.string.alert_dialog_message
+
+            positiveButton {
+                textResourceId = android.R.string.ok
+                onClickListener = {
+                    updateResultDialog(MESSAGE_CLICKED_ON_POSITIVE_BUTTON)
+                }
+            }
+
+            negativeButton {
+                textResourceId = android.R.string.cancel
+                onClickListener = {
+                    updateResultDialog(MESSAGE_CLICKED_ON_NEGATIVE_BUTTON)
+                }
+            }
+        }
+    }
+
+
+    private fun displayAlertDialogWithDismissCallback() {
+        resetDialogResult()
+
+        showDialog {
+            messageResourceId = R.string.alert_dialog_message
+
+            positiveButton {
+                textResourceId = android.R.string.ok
+                onClickListener = {
+                    updateResultDialog(MESSAGE_CLICKED_ON_POSITIVE_BUTTON)
+                }
+            }
+
+            negativeButton {
+                textResourceId = android.R.string.cancel
+                onClickListener = {
+                    updateResultDialog(MESSAGE_CLICKED_ON_NEGATIVE_BUTTON)
+                }
+            }
+
+            onDismissListener = {
+                updateResultDialog(MESSAGE_DIALOG_DISMISSED)
+            }
+        }
+    }
+
+    private fun displayAlertDialogWithCancelCallback() {
+        resetDialogResult()
+
+        showDialog {
+            messageResourceId = R.string.alert_dialog_message
+
+            positiveButton {
+                textResourceId = android.R.string.ok
+                onClickListener = {
+                    updateResultDialog(MESSAGE_CLICKED_ON_POSITIVE_BUTTON)
+                }
+            }
+
+            negativeButton {
+                textResourceId = android.R.string.cancel
+                onClickListener = {
+                    updateResultDialog(MESSAGE_CLICKED_ON_NEGATIVE_BUTTON)
+                }
+            }
+
+            onCancelListener = {
+                updateResultDialog(MESSAGE_DIALOG_CANCELED)
+            }
+        }
+    }
+
+
+    private fun displayAlertDialogWithCustomView() {
+        resetDialogResult()
+
+        val layoutInflater = LayoutInflater.from(this)
+
+        val view = layoutInflater.inflate(R.layout.dialog_custom_view, null)
+
+        val dialog = buildDialog {
+            customView = view
+        }
+
+        val customViewButton: Button = view.findViewById(R.id.buttonCustomView)
+
+        customViewButton.setOnClickListener {
+            updateResultDialog(MESSAGE_CUSTOM_VIEW_INTERACTION)
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
+    private fun updateResultDialog(resultText: String) {
+        textViewDialogResult.text = resultText
+    }
+
+    private fun resetDialogResult() {
+        updateResultDialog("-")
+    }
+}
